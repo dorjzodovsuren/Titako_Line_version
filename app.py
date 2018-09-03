923044b4cd9ea9c7fa5986f09c66c0cdb6b4380d
Dorjzodovsuren <37056830+dorjzodovsuren@users.noreply.github.com>
2018-09-03 16:21:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def hello():
         if request.args.get("hub.verify_token") == VERIFY_TOKEN:
             return request.args.get("hub.challenge")
         else:
-            return 'Invalid verification token'
+            return 'This website is under construction !!!'
 
     if request.method == 'POST':
         output = request.get_json()
