a0cd5f2ec0a73d39c586244ad317d1d1c05d4d4c
Dorjzodovsuren <37056830+dorjzodovsuren@users.noreply.github.com>
2018-09-29 03:34:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -171,11 +171,11 @@ def handle_text_message(event):
     elif text1 == 'instruction':
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(text='English version', title='Instruction', actions=[
-                URIAction(label='Go to Instruction', uri='https://docs.google.com/presentation/d/1BS24qsVaKE6t6h3jTANEpKOydivOjvdmFFXnAHRJ3Ww/edit?usp=sharing'),
+                URIAction(label='Go to Instruction', uri='https://www.dropbox.com/s/rn9femnnywl5xwt/Instruction%20TiTaX.pdf?raw=1'),
                 PostbackAction(label='Comment', data='comment')
             ]),
             CarouselColumn(text='Japanese version', title='Instruction', actions=[
-                URIAction(label='Go to Instruction', uri='https://www.dropbox.com/s/xjvc90mbxvfs41k/Instruction.pdf?raw=1'),
+                URIAction(label='Go to Instruction', uri='https://www.dropbox.com/s/rn9femnnywl5xwt/Instruction%20TiTaX.pdf?raw=1'),
                 PostbackAction(label='Comento', data='comment')
             ]),
             CarouselColumn(text='Mongolian version', title='Instruction', actions=[
